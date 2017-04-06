a711d4f3ac82bfa9bd2764b747a4a5a3705126de
Norton Pengra <npengra317@gmail.com>
2017-04-06 14:25:38 -0700

    update settings to include new app

--- src/webroles/dashboard/models.py
+++ src/webroles/dashboard/models.py
@@ -4,9 +4,11 @@ import json
 
 # Create your models here.
 
+
 class TimeStampMixin(object):
     date_created = models.TimeDate(auto_now_add=True)
     date_modified = models.TimeDate(auto_now=True)
+
 
 class JsonResultMixin(object):
     result = models.TextField()
@@ -19,15 +21,18 @@ class JsonResultMixin(object):
 class Question(models.Model, TimeStampMixin):
     question = models.CharField(unique=True, max_length=255)
 
+
 class Answer(models.Model, TimeStampMixin):
     answer = models.CharField(max_length=255)
     question = models.ForeignKey(related_name="answers")
+
 
 class UserResponse(models.Model, TimeStampMixin, JsonResultMixin):
     user = models.ForeignKey(User)
     
     def similarity(self, diseaseModel):
         return 0
+
 
 class _DiseaseModel(models.Model, TimeStampMixin, JsonResultMixin):
     name = models.CharField(unique=True, max_length=255)
--- src/webroles/sequencehack/settings.py
+++ src/webroles/sequencehack/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '2)a#sphjuvkkv6@@b2+a)+npb=w(9pioeokt3he!6d1!p$3vxy'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'dashboard',
+    # 'api'
 ]
 
 MIDDLEWARE = [
