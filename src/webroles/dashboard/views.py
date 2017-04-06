13c5a03e4d67c421924ac6341422239cbef644fd
Norton Pengra <npengra317@gmail.com>
2017-04-06 15:00:27 -0700

    create dummy question api

--- src/webroles/dashboard/views.py
+++ src/webroles/dashboard/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.views import View
+from dashboard import models
+import json
 
 # Create your views here.
+
+class QuestionAPI(View):
+    
+    # TODO: Replace this with an actual API implementation
+    def get(self, request):
+        return_blob = {}
+        for question in models.Question.objects.all():
+            return_blob[question.pk] = {
+                "question": str(question),
+                "answers": [_.answer for _ in question.answers.all()]
+            }
+        return HttpResponse(json.dumps(return_blob, indent=2))
+
+    
