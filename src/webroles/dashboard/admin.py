9b87b85e22c2e3b15bf664e053ef5fda1d45e2ff
Norton Pengra <npengra317@gmail.com>
2017-04-06 14:59:56 -0700

    update admin page

--- src/webroles/dashboard/admin.py
+++ src/webroles/dashboard/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from dashboard import models
 
 # Register your models here.
+
+admin.site.register(models.Question)
+admin.site.register(models.Answer)
+admin.site.register(models.UserResponse)
