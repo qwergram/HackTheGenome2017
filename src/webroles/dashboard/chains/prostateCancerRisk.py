767aadeb62da8ad9f546d77d445961849aac8213
Norton Pengra <npengra317@gmail.com>
2017-04-07 11:04:48 -0700

    create prostateCancerRisk Chain

---
+++ src/webroles/dashboard/chains/prostateCancerRisk.py
@@ -0,0 +1,44 @@
+try:
+    from dashboard.chains.UsageBase import ChainWrapper
+    from dashboard.secrets import *
+except ImportError:
+    from UsageBase import ChainWrapper
+    TOKEN = None
+from django.views import View
+from django.http import HttpResponse
+
+import json
+
+def _result(token, fileId):
+    payload = ChainWrapper(token, 'Chain1333', fileId).get_report()
+    return_val = {}
+    for item in payload:
+        return_val[item.getName()] = item.getValue().getData()
+    print(return_val)
+    return return_val
+
+
+class ProstateCancerRiskView(View):
+    
+    def get(self, request):
+        return HttpResponse(json.dumps(_result(TOKEN, request.GET['fileid'])))
+        
+        response = json.dumps(_result(TOKEN, request.GET['fileid']), indent=2)
+        qualitative = json.loads(response.lower())['riskdescription']
+        if 'low' in qualitative:
+            return HttpResponse('{"value": 1}')
+        if 'normal' in qualitative:
+            return HttpResponse('{"value": 2}')
+        if 'increased' in qualitative:
+            return HttpResponse('{"value": 2.5}')
+        if 'moderate' in qualitative:
+            return HttpResponse('{"value": 5}')
+        if 'very hi' in qualitative:
+            return HttpResponse('{"value": 20}')
+        if 'hi' in qualitative:
+            return HttpResponse('{"value": 15}')
+
+
+if __name__ == "__main__":
+    x = _result('2af4f10acac15b8acf577360d5825616898bcd72', '227679')
+    import pdb; pdb.set_trace()
