af45508d9cb13cf13211dfb67cc5751636a3c498
Norton Pengra <npengra317@gmail.com>
2017-04-07 09:31:11 -0700

    make creating json blobs easier with new admin inter

--- src/webroles/dashboard/admin.py
+++ src/webroles/dashboard/admin.py
@@ -3,7 +3,25 @@ from dashboard import models
 
 # Register your models here.
 
+
+class AnswerAdmin(admin.ModelAdmin):
+    list_display = ('answer_pk', 'question_pk', 'answer_str')
+
+    def answer_str(self, obj):
+        return str(obj)
+    answer_str.short_description = "Answer Model"
+
+    def question_pk(self, obj):
+        return str(obj.question.pk)
+    question_pk.short_description = "Q#"
+
+    def answer_pk(self, obj):
+        return str(obj.pk)
+    answer_pk.short_description = "A#"
+
+
 admin.site.register(models.Question)
-admin.site.register(models.Answer)
+admin.site.register(models.Answer, AnswerAdmin)
 admin.site.register(models.UserResponse)
 admin.site.register(models.FeedBackModel)
+admin.site.register(models.DiseaseModel)
